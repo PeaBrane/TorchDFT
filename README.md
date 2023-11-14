c8fc48923cffe6d5130a97e1b68555fa94cf87a6
ypei <ypei@brainchipinc.com>
2023-11-14 11:40:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This repository is dedicated to exploring and implementing the Discrete Fourier Transform (DFT) in various ways using PyTorch.
+The current goal is mostly educational, therefore the implementations may not always be the most optimal, 
+but they are designed to provide clear insights into the workings of DFT algorithms.
+
+In the near future, there are plans to optimize these implementations to interact better with kernel fusion and compilation
+to provide actual speed ups when implementing DFTs on GPUs.
---
+++ fft_einsum.py
@@ -0,0 +1,45 @@
+import einops
+import torch
+from torch.backends import opt_einsum
+
+assert opt_einsum.is_available()
+opt_einsum.strategy = 'optimal'
+
+
+TAU = 2 * torch.pi * 1.j
+
+
+def dft_einsum(input):
+    """Applies an order-3 FFT on an input signal, 
+    where the first two hierarchies are radix-16.
+
+    Args:
+        input (torch.Tensor): the input signal
+
+    Returns:
+        torch.Tensor: the DFT'd signal
+    """
+    input_block = einops.rearrange(input, '... (length_1 length_2 length_3) -> ... length_1 length_2 length_3', 
+                                   length_1=16, length_2=16)
+    last_dim_size = input_block.shape[-1]
+    
+    range_16 = torch.arange(16, device=input.device)
+    range_small = torch.arange(last_dim_size, device=input.device)
+    range_large = torch.arange(16 * last_dim_size, device=input.device)
+    
+    dft = torch.exp(-(range_16.unsqueeze(-1) * range_16) / 16 * TAU)
+    dft_small = torch.exp(-(range_small.unsqueeze(-1) * range_small) / last_dim_size * TAU)
+    
+    twid = torch.exp(-(range_16.unsqueeze(-1) * range_large) / (16 ** 2 * last_dim_size) * TAU).reshape(16, 16, -1)
+    twid_small = torch.exp(-(range_16.unsqueeze(-1) * range_small) / (16 * last_dim_size) * TAU)
+    
+    return torch.einsum('...xyz,xf,yg,zh,fyz,gz->...hgf', 
+                        input_block.to(torch.cfloat), 
+                        dft, dft, dft_small, 
+                        twid, twid_small).flatten(-3, -1)
+
+
+input = torch.rand(2, 4, 1024)
+input_f = torch.fft.fft(input)
+
+assert torch.allclose(input_f, dft_einsum(input), rtol=1e-3, atol=1e-3)
